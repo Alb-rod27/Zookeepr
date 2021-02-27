0941e2981578d431987b6f0dc033f325d940963a
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-02-27 12:32:38 -0800

    integrated animals and zookeeper js

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
+app.use(express.static('zookeepr-public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -101,6 +100,18 @@ app.post('/api/animals', (req, res) => {
 });
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './zookeepr-public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './zookeepr-public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './zookeepr-public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './zookeepr-public/index.html'));
 });
 
